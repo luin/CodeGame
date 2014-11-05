eae23444efd355aa3493521dad50bb323a5186a1
Zihua Li <i@zihua.li>
2014-11-05 23:50:25 +0800

    Change TEXT to LONGTEXT

--- models/result.js
+++ models/result.js
@@ -2,7 +2,7 @@ module.exports = function(DataTypes) {
   return [{
     user1:    DataTypes.INTEGER,
     user2:    DataTypes.INTEGER,
-    record:   DataTypes.TEXT,
+    record:   { type: DataTypes.TEXT, big: true },
     winner:   DataTypes.INTEGER
   }];
 };
