76ef19158732a9b889148e3b141d1811c49ba874
Zihua Li <i@zihua.li>
2014-12-02 17:33:38 +0800

    使用 Sandbox 包裹函数，防止全局函数带来的安全问题

--- sandbox/game/sandbox.js
+++ sandbox/game/sandbox.js
@@ -1,11 +1,20 @@
 var Sandbox = module.exports = function(sandbox) {
+  var sandMethod = function(method, thisObj) {
+    if (typeof thisObj === 'undefined') {
+      thisObj = null;
+    }
+    return function() {
+      return method.apply(thisObj, arguments);
+    };
+  };
   var math = this.Math = {};
   ['abs', 'acos', 'asin', 'atan', 'atan2', 'ceil',
   'cos', 'exp', 'floor', 'log', 'max', 'min', 'pow', 'round', 'sin', 'sqrt', 'tan'].forEach(function(method) {
-    math[method] = Math[method];
+    math[method] = sandMethod(Math[method]);
   });
 
-  this.parseInt = parseInt;
+  this.parseInt = sandMethod(parseInt);
+  this.parseFloat = sandMethod(parseFloat);
 
   for (var key in sandbox) {
     if (sandbox.hasOwnProperty(key)) {
