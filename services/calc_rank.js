101326892ec11c24c2f8fb43038036f8b997110d
Zihua Li <i@zihua.li>
2014-12-07 21:40:19 +0800

    过滤短 AI 代码以加速排名

--- services/calc_rank.js
+++ services/calc_rank.js
@@ -8,82 +8,76 @@ var aqsort = require('aqsort');
 var runCodes = require('./util');
 
 var MAP_ID;
-
 var round = 0;
 
-var benchmarkAI = require('./benchmark_ai').toString();
 var calc = module.exports = function(end) {
   var startTime = new Date();
   Code.findAll().done(function(err, codeResult) {
-    async.filterSeries(codeResult, function(code, callback) {
-      Game(MAP_ID, code.code, benchmarkAI, { cache: false }, function(err, result) {
-        console.log(MAP_ID, code.UserId, result.meta.result.winner, result.meta.result.reason);
-        callback(result.meta.result.winner === 0 &&
-                 ['crashed', 'star'].indexOf(result.meta.result.reason) > -1);
-      });
-    }, function(codeResult) {
-      console.log('Valid codes: ' + codeResult.length);
-      aqsort(codeResult.map(function(item) {
-        item = item.dataValues;
+    codeResult = codeResult.filter(function(item) {
+      return item.code.length > 700;
+    });
+    console.log('Valid codes: ' + codeResult.length);
+    aqsort(codeResult.map(function(item) {
+      item = item.dataValues;
+      item.win = item.lose = 0;
+      item.winReasons = {};
+      item.loseReasons = {};
+      return item;
+    }), function(a, b, callback) {
+      round += 1;
+      runCodes(MAP_ID, a, b, callback);
+    }, function(err, result) {
+      var C = [];
+      var top = result.splice(0, 30);
+      top.forEach(function(item) {
         item.win = item.lose = 0;
         item.winReasons = {};
         item.loseReasons = {};
-        return item;
-      }), function(a, b, callback) {
-        round += 1;
-        runCodes(MAP_ID, a, b, callback);
-      }, function(err, result) {
-        var C = [];
-        var top = result.splice(0, 30);
-        top.forEach(function(item) {
-          item.win = item.lose = 0;
-          item.winReasons = {};
-          item.loseReasons = {};
-        });
-        var min = top.length > 30 ? 30 : top.length;
-        for (var i = 0; i < min; ++i) {
-          for (var j = i + 1; j < min; ++j) {
-            C.push([i, j]);
-          }
+      });
+      var min = top.length > 30 ? 30 : top.length;
+      for (var i = 0; i < min; ++i) {
+        for (var j = i + 1; j < min; ++j) {
+          C.push([i, j]);
         }
-        async.eachSeries(C, function(item, next) {
-          runCodes(MAP_ID, top[item[0]], top[item[1]], next);
-        }, function() {
-          result = top.sort(function(a, b) {
-            return b.win - a.win;
-          }).concat(result);
-          result.forEach(function(item, index) {
-            item.rank = index + 1;
-            item.winReason = maxReason(item.winReasons);
-            item.loseReason = maxReason(item.loseReasons);
-          });
-          Code.update({
-            rank: null,
-            win: 0,
-            lost: 0,
-            winReason: '',
-            loseReason: ''
-          }, { where: {} }).done(function() {
-            async.eachLimit(result, 10, function(item, next) {
-              Code.update({
-                rank: item.rank,
-                win: item.win,
-                lost: item.lose,
-                winReason: item.winReason,
-                loseReason: item.loseReason,
-              }, {
-                where: { UserId: item.UserId }
-              }).done(next);
-            }, function() {
-              console.log('Done(' + round + '): ' + (new Date() - startTime));
-              if (end) {
-                end();
-              }
-            });
+      }
+      async.eachSeries(C, function(item, next) {
+        runCodes(MAP_ID, top[item[0]], top[item[1]], next);
+      }, function() {
+        result = top.sort(function(a, b) {
+          return b.win - a.win;
+        }).concat(result);
+        result.forEach(function(item, index) {
+          item.rank = index + 1;
+          item.winReason = maxReason(item.winReasons);
+          item.loseReason = maxReason(item.loseReasons);
+        });
+        Code.update({
+          rank: null,
+          win: 0,
+          lost: 0,
+          winReason: '',
+          loseReason: ''
+        }, { where: {} }).done(function() {
+          async.eachLimit(result, 10, function(item, next) {
+            Code.update({
+              rank: item.rank,
+              win: item.win,
+              lost: item.lose,
+              winReason: item.winReason,
+              loseReason: item.loseReason,
+            }, {
+              where: { UserId: item.UserId }
+            }).done(next);
+          }, function() {
+            console.log('Done(' + round + '): ' + (new Date() - startTime));
+            if (end) {
+              end();
+            }
           });
         });
       });
     });
+    // });
   });
 };
 
